b8d03b465052f9e6faa1bffea22eb9d130329a44
guodh1 <guodh1@yonyou.com>
2021-04-16 02:54:54 +0800

    人员列表

--- src/pages/user/list.js
+++ src/pages/user/list.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ajax from "../../api/ajax";
+import {Link} from "umi";
+import {Button, Space, Table, Tag} from "antd";
+import appCss from "../App.css";
 
 export default class UserList  extends React.Component {
   state = {
@@ -12,6 +15,19 @@ export default class UserList  extends React.Component {
 
   }
 
+  getButton(record){
+    let a=record.key
+    console.log("record")
+    console.log(record)
+    switch (record.ban) {
+      case '0':
+        return<Button type="link" onClick={(e)=>this.closs(a,e)}>封禁用户</Button>
+      case '-1':
+        return<Button type="link" onClick={(e)=>this.open(a,e)}>解禁用户</Button>
+    }
+  }
+
+
   async getData(){
     ajax("/api/user/getAll",{},'GET').then(
       (q)=>{
@@ -23,8 +39,78 @@ export default class UserList  extends React.Component {
   }
 
   render() {
+    const columns = [
+      {
+        title: '用户名称',
+        dataIndex: 'name',
+        key: 'name',
+        className:"appCss.table-row",
+        render: (text,record,index) => <Link  to={"/index/printer/card?id="+record.key}>{text}</Link>,
+      },
+      {
+        title: '用户组',
+        key: 'role',
+        dataIndex: 'role',
+        render: tags => (
+          <>
+            {tags.map(tag => {
+              let color = 'geekblue';
+
+              return (
+                <Tag color={color} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </>
+        ),
+
+      },
+      {
+        title: '用户状态',
+        dataIndex: 'ban',
+        key: 'ban',
+        render: tag =>
+          {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          let value=""
+          switch (tag) {
+          case '0':
+            color = 'green'
+            value='正常'
+            break;
+          case '-1':
+            color = 'volcano'
+            value='封禁'
+            break;
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {value}
+            </Tag>
+          );
+        }
+      },
+      {
+        title: '操作',
+        key: 'action',
+        render: (text, record,index) => (
+          <Space size="middle" >
+            <Link  to={"/index/printer/card?id="+record.key}>详情</Link>
+            {this.getButton(record)}
+          </Space>
+        ),
+      },
+    ];
+
     return(
-      <div>user list</div>
+      <div className={appCss.App}>
+        <Table columns={columns}
+               rowKey={(record) => {
+                 return (record.pkUser + Date.now()) //在这里加上一个时间戳就可以了
+               }}
+               dataSource={this.state.data.obj} />
+      </div>
     )
   }
 }
